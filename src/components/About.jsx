4fdda8ce32a67ab17830044721b9369262808d29
luq-mne <luqman.yazrie@outlook.com>
2024-12-01 15:59:55 +0800

    uhh some bug with blog button

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,13 @@
 import KualaLumpur from "../assets/kuala-lumpur.jpg"
 import Books from "../assets/books.jpg"
+import pfp from "../assets/pfp.jpg"
 
 export default function About() {
     return (
         <div>
-            
+            <div class="p-2 bg-white col col-span-10">
+                <img src={pfp}></img>
+            </div>
         </div>
     )
 }
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,15 @@
 import background from "../assets/background.jpg"
 import backgroundMobile from "../assets/background-mobile.jpg"
+import openIcon from "../assets/icons/open.svg"
 
 export default function Hero() {
     return (
-        <div className="relative">
+        <div className="relative space-y-2">
             <picture>
                 <source media="(max-width: 768px)" srcSet={backgroundMobile} />
                 <img src={background} class="w-full h-full object-cover"/>
             </picture>
+            
             <div className="absolute inset-0 flex">
                 <div class="p-4">
                     <h1 class="text-4xl lg:text-6xl text-[rgb(255,255,255)] font-semibold">
@@ -19,6 +21,24 @@ export default function Hero() {
                     </p>
                 </div>
             </div>
+
+            <div class="p-2 bg-white flex flex-row justify-between items-center">
+                <h1 class="text-black text-base lg:text-xl">
+                    Posts on various topics, including my personal thoughts
+                </h1>
+
+                <button
+                    onClick={() => window.open('/blog', '_blank')}
+                    class="p-2 px-4 hover:bg-[rgb(25,25,25)] duration-300 flex space-x-2 items-center bg-black">
+                    <p class="text-base lg:text-xl text-white font-semibold">
+                        Blog
+                    </p>
+
+                    <img src={openIcon} class="h-4 w-4 invert" />
+                </button>
+            </div>
+
+            <hr className="border-t-2 my-4 border-[rgb(25,25,25)]" />
         </div>
     )
 }
