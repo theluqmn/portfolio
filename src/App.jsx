77680108d6e982b7a260f82a24ab3b809c3434ec
luq-mne <luqman.yazrie@outlook.com>
2024-12-01 19:16:12 +0800

    finished on about and location

--- src/App.jsx
+++ src/App.jsx
@@ -18,11 +18,11 @@ export default function App() {
             <header class="p-2 text-xl flex flex-col fixed z-10 bg-[url('./assets/background-header.jpg')] bg-cover bg-center w-full backdrop-blur-sm">
                 <div class="w-full md:w-8/12 mx-auto flex items-center justify-between gap-3">
                     <div class="flex items-center gap-3 flex-1">
-                        <button class="p-2 bg-white hover:bg-[rgb(200,200,200)] duration-500">
+                        <button class="p-2 bg-white hover:bg-[rgb(220,220,220)] duration-500">
                             <img src={listIcon} alt="list icon" class="w-6 h-6" />
                         </button>
 
-                        <div class="p-1 flex items-center gap-3 bg-white hover:bg-gray-200 duration-500 flex-1">
+                        <div class="p-1 flex items-center gap-3 bg-white hover:bg-[rgb(220,220,220)] duration-500 flex-1">
                             <img src={pfp} alt="pfp" class="w-8 h-8 object-cover" />
 
                             <div class="flex gap-2 items-center">
@@ -37,7 +37,7 @@ export default function App() {
                         </div>
                     </div>
 
-                    <div class="p-2 flex items-center gap-4 bg-white text-[rgb(0,0,0)] hover:bg-gray-200 duration-500">
+                    <div class="p-2 flex items-center gap-4 bg-white text-[rgb(0,0,0)] hover:bg-[rgb(220,220,220)] duration-500">
                         <a href="https://x.com/theluqmn" target="_blank">
                             <img src={xIcon} class="w-6 h-6 hover:scale-105 duration-300"/>
                         </a>
@@ -56,6 +56,7 @@ export default function App() {
             {/* Content */}
             <div class="pt-16 p-2 md:p-1 md:pt-16 lg:w-8/12 items-center mx-auto space-y-3 text-sm lg:text-base">
                 <Hero/>
+
                 <div class="p-2 bg-white flex flex-row justify-between items-center">
                     <h1 class="text-black text-base lg:text-xl">
                         Posts on various topics, including my personal thoughts
@@ -63,12 +64,12 @@ export default function App() {
 
                     <button
                         onClick={() => window.open('/blog', '_blank')}
-                        class="p-2 px-4 hover:bg-[rgb(25,25,25)] duration-300 flex space-x-2 items-center bg-black">
+                        class="p-2 px-4 hover:bg-[rgb(35,35,35)] duration-300 flex space-x-2 items-center bg-black">
                         <p class="text-base lg:text-xl text-white">
                             Blog
                         </p>
                     
-                        <img src={openIcon} class="h-4 w-4 invert" />
+                        <img src={openIcon} class="h-3 w-3 lg:h-4 lg:w-4 invert" />
                     </button>
                 </div>
 
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,11 +2,51 @@ import KualaLumpur from "../assets/kuala-lumpur.jpg"
 import Books from "../assets/books.jpg"
 import pfp from "../assets/pfp.jpg"
 
+import ImageFrame from "./ImageFrame"
+
 export default function About() {
     return (
-        <div>
-            <div class="p-2 bg-white col col-span-10">
-                <img src={pfp}></img>
+        <div class="space-y-3">
+            <div class="flex flex-col gap-4">
+                <p>
+                    Howdy, I am <b class="font-semibold text-white">Luqman</b> (also goes by Luq),
+                    just someone who does coding for fun, amongst other things.
+                </p>
+
+                <p>
+                    Why "The Renaissance Man"? I turned my trouble specializing to my advantage,
+                    by deciding to learn a little bit of everything. Of course I have my own set
+                    of specialities, but I dabbling in other fields is just as fun.
+                </p>
+
+                <p class="text-xs text-[rgb(150,150,150)]">
+                    Also it is because I find it a cool term lol, so why not?
+                </p>
+            </div>
+
+            <div class="pb-32 grid grid-cols-1 lg:grid-cols-2 gap-4">
+                <ImageFrame src={KualaLumpur} alt="Kuala Lumpur, Malaysia">
+                    <div class="flex flex-col gap-2 text-left">
+                        <h1 class="text-base lg:text-xl text-gray-300">
+                            Currently based in
+                        </h1>
+                        <h1 class="text-xl lg:text-2xl font-semibold">
+                            Kuala Lumpur, Malaysia
+                        </h1>
+                    </div>
+                </ImageFrame>
+
+                <ImageFrame src={Books} alt="Books">
+                    <div class="flex flex-col gap-2 text-left lg:text-right">
+                        <h1 class="text-base lg:text-xl text-gray-300">
+                            Living as a
+                        </h1>
+
+                        <h1 class="text-xl lg:text-2xl font-semibold">
+                            Student
+                        </h1>
+                    </div>
+                </ImageFrame>
             </div>
         </div>
     )
