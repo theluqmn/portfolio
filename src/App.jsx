413c19ed9cb6d7f58a5d54b3db5d010547dd6854
luq-mne <luqman.yazrie@outlook.com>
2024-12-01 14:17:23 +0800

    header is now looking good

--- src/App.jsx
+++ src/App.jsx
@@ -4,58 +4,50 @@ import listIcon from "./assets/icons/list.svg"
 import pfp from "./assets/pfp.jpg"
 import openIcon  from "./assets/icons/open.svg"
 
+import xIcon from "./assets/icons/x.svg"
+import instagramIcon from "./assets/icons/instagram.svg"
+import githubIcon from "./assets/icons/github.svg"
+
+
 export default function App() {
-    const [isMenuOpen, setIsMenuOpen] = createSignal(false);
-
-    const handleClickOutside = (e) => {
-        if (!e.target.closest('button')) {
-            setIsMenuOpen(false);
-        }
-    };
-
     return (
-        <div onClick={handleClickOutside} class="bg-gradient-r from-[rgb(0,0,0)] to-[rgb(50,50,50)]">
-            <header class="p-2 text-xl flex flex-col fixed z-10 bg-[rgb(255,255,255)] w-full backdrop-blur-sm">
-                {/* Header */}
-                <div class="w-full md:w-8/12 mx-auto flex items-center justify-between">
-                    <div class="flex items-center">
-                        <button 
-                            class="mr-4 p-2 duration-300 hover:bg-gray-100 rounded-lg"
-                            onClick={() => setIsMenuOpen(!isMenuOpen())}
-                        >
-                            <img src={listIcon} class={`w-6 h-6 transition-transform duration-500 ${isMenuOpen() ? 'rotate-90' : ''}`} />
+        <div>
+            <header class="p-2 text-xl flex flex-col fixed z-10 bg-[rgb(82,157,242)] w-full backdrop-blur-sm">
+                <div class="w-full md:w-8/12 mx-auto flex items-center justify-between gap-3">
+                    <div class="flex items-center gap-3 flex-1">
+                        <button class="p-2 bg-white outline outline-2">
+                            <img src={listIcon} alt="list icon" class="w-6 h-6" />
                         </button>
 
-                        <div class="flex gap-2 items-center">
-                            <img src={pfp} class="w-8 h-8 rounded-full object-cover" alt="luqman profile picture"/>
-                            
-                            <div class="flex gap-2 items-center">
-                                <h1 class="text-2xl font-semibold text-black">
-                                    Luqman
-                                </h1>
+                        <div class="p-1 flex items-center gap-3 bg-white hover:bg-gray-200 duration-500 outline outline-2 flex-1">
+                            <img src={pfp} alt="pfp" class="w-8 h-8 object-cover" />
 
-                                <h1 class="text-base text-[rgb(25,25,25)]">
-                                    (Portfolio)
-                                </h1>
-                            </div>
+                            <h1 class="font-semibold text-black">
+                                Luqman
+                            </h1>
                         </div>
                     </div>
-                </div>
 
-                {/* Collapsible Menu */}
-                <div class={`w-full md:w-8/12 mx-auto overflow-hidden transition-all duration-500 ${isMenuOpen() ? 'max-h-10' : 'max-h-0'}`}>
-                    <div class="flex gap-2 overflow-x-auto whitespace-nowrap">
-                        <a href="https://github.com/theluqmn/portfolio" class="text-base block px-4 py-2 duration-300 hover:bg-gray-100 rounded-lg">
-                            Repository
+                    <div class="p-2 flex items-center gap-4 bg-white text-[rgb(0,0,0)] hover:bg-gray-200 duration-500 outline outline-2">
+                        <a href="https://x.com/theluqmn" target="_blank">
+                            <img src={xIcon} class="w-6 h-6 hover:scale-110 duration-300"/>
                         </a>
 
-                        <a href="https://theluqmn.github.io/blog" class="items-center text-base block px-4 py-2 duration-300 hover:bg-gray-100 rounded-lg">
-                            Blog
-                            <img src={openIcon} class="w-3 h-3 inline-block ml-1"/>
+                        <a href="https://instagram.com/theluqmn" target="_blank">
+                            <img src={instagramIcon} class="w-6 h-6 hover:scale-110 duration-300"/>
+                        </a>
+
+                        <a href="https://github.com/theluqmn" target="_blank">
+                            <img src={githubIcon} class="w-6 h-6 hover:scale-110 duration-300"/>
                         </a>
                     </div>
                 </div>
             </header>
+
+            {/* Content */}
+            <div class="pt-16 p-2 lg:w-8/12 items-center mx-auto space-y-4 text-sm lg:text-base">
+                <h1>Hello</h1>
+            </div>
         </div>
     )
 }
